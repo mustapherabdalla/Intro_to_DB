dc6398b7eaaae81c1894d13e20ab5e92f25b2339
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-08-14 19:08:13 +0300

    Populating the database with my very first data

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+USE alx_book_store;
+INSERT INTO
+    customers(customers.customer_id, customers.customer_name, customers.email, customers.address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
