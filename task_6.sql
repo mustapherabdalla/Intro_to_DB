f38f71d42e259d6650c16f5fde0d1a7897fa423e
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-08-14 19:19:31 +0300

    Adding multiple rows in the database

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 INSERT INTO customers(customers.customer_id, customers.customer_name, customers.email, customers.address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-       (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+       (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
