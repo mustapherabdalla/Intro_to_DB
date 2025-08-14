3458c2b0e4536b25c8b348a8c2aff510dca0be30
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-08-14 18:34:43 +0300

    Full description

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,13 @@
 USE alx_book_store;
-SELECT * FROM books;
+SELECT
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    information_schema.columns
+WHERE
+    table_schema = DATABASE()
+    AND table_name = 'Books';
